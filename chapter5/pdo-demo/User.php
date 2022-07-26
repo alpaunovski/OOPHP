4e3e91ed2f4ff6816f755331905b29d3f118b6fe
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-07-26 08:07:34 +0300

    pdo

--- chapter5/pdo-demo/User.php
+++ chapter5/pdo-demo/User.php
@@ -1,9 +1,51 @@
 <?php
 
+namespace App\Models;
+
 class User
 {
     private $id;
     private $name;
     private $email;
-    private $created_at;
+    private \DateTime $created_at;
+}
+
+public function getId(): int
+{
+    return $this->id;
+}
+
+public function setId (int $id): void
+{
+    $this->id = $id;
+}
+
+public function getName(): string
+{
+    return $this->name;
+}
+
+public function setName(string $name): void
+{
+    $this->name = $name;
+}
+
+public function getEmail(): string
+{
+    return $this->email;
+}
+
+public function setName(string $email): void
+{
+    $this->email = $email;
+}
+
+public function getCreatedAt(): \DateTime
+{
+    return $this->created_at;
+}
+
+public function setCreatedAt(\DateTime $created_at): void
+{
+    $this->created_at = $created_at;
 }
